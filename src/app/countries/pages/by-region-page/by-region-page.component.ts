641406e0baa634f310d48fff35cdc5de63dc5689
unknown <lienapuey@gmail.com>
2024-03-11 17:57:52 +0100

    localstorage

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
+
 
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit{
 
   public region: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService){}
 
+  ngOnInit(): void {
+    this.region = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
-  searchByRegion( term : string ){
+  searchByRegion( term : Region ){
+    this.selectedRegion = term; 
+    
     this.countriesService.searchRegion(term)
       .subscribe( region => this.region = region);
   }    
